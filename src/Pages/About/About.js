2f346662f7165c53ea0d9eef4cf51d906ff3d5aa
mdrokib231185 <mdrokhasan231185@gmail.com>
2022-05-09 12:39:16 +0600

    hooks add

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,55 +1,55 @@
-import React from 'react';
+// import React from 'react';
 
-const About = () => {
-      return (
-        <div>
-          <h3>1. Difference between javascript and nodejs</h3>
-          <div className="d-flex mx-5">
-            <div>
-              {" "}
-              <h1>JavaScript</h1>
-              <p>
-                জাভাস্ক্রিপ্ট একটি প্রোগ্রামিং ভাষা যা ওয়েবসাইটে স্ক্রিপ্ট
-                লেখার জন্য ব্যবহৃত হয়।জাভাস্ক্রিপ্ট শুধুমাত্র ব্রাউজারে চালানো
-                যেতে পারে। এটি মূলত ক্লায়েন্ট-সাইডে ব্যবহৃত হয়। জাভাস্ক্রিপ্ট
-                এইচটিএমএল যোগ করতে যথেষ্ট সক্ষম।জাভাস্ক্রিপ্ট যেকোন ব্রাউজার
-                ইঞ্জিনে চলতে পারে যেমন সাফারিতে জেএস কোর এবং ফায়ারফক্সে
-                স্পাইডারমনকি।
-              </p>
-            </div>
-            <div>
-              <h1>Nodejs</h1>
-              <p>
-                নোডজেএস একটি জাভাস্ক্রিপ্ট রানটাইম পরিবেশ| আমরা NodeJS এর
-                ​​সাহায্যে ব্রাউজারের বাইরে Javascript চালাতে পারি। এটি
-                বেশিরভাগই সার্ভার-সাইডে ব্যবহৃত হয়। এটি বেশিরভাগই সার্ভার-সাইডে
-                ব্যবহৃত হয়। Nodejs এর HTML ট্যাগ যোগ করার ক্ষমতা নেই। V8 হল
-                node.js এর ভিতরে জাভাস্ক্রিপ্ট ইঞ্জিন যা জাভাস্ক্রিপ্ট পার্স করে
-                এবং চালায়।
-              </p>
-            </div>
-          </div>
-          <h1>2.SQL vs NoSQL Database </h1>
-          <p>
-            এসকিউএল ডাটাবেসগুলি সম্পর্কযুক্ত, নোএসকিউএল ডেটাবেসগুলি সম্পর্কহীন।
-            SQL ডাটাবেস স্ট্রাকচার্ড কোয়েরি ভাষা ব্যবহার করে এবং একটি
-            পূর্বনির্ধারিত স্কিমা আছে। নোএসকিউএল ডাটাবেসে অসংগঠিত ডেটার জন্য
-            গতিশীল স্কিমা রয়েছে। এসকিউএল ডাটাবেসগুলি উল্লম্বভাবে স্কেলযোগ্য,
-            যখন নোএসকিউএল ডেটাবেসগুলি অনুভূমিকভাবে স্কেলযোগ্য। এসকিউএল
-            ডাটাবেসগুলি টেবিল-ভিত্তিক, যখন নোএসকিউএল ডাটাবেসগুলি নথি, কী-মান,
-            গ্রাফ, বা প্রশস্ত-কলাম স্টোর। এসকিউএল ডাটাবেসগুলি বহু-সারি লেনদেনের
-            জন্য ভাল, যখন নথি বা JSON-এর মতো অসংগঠিত ডেটার জন্য NoSQL ভাল।
-          </p>
-          <h1>3.What is the purpose of jwt and how does it work ?</h1>
-          <p>
-            JWT, or JSON Web Token, is an open standard used to share security
-            information between two parties — a client and a server. Each JWT
-            contains encoded JSON objects, including a set of claims. JWTs are
-            signed using a cryptographic algorithm to ensure that the claims
-            cannot be altered after the token is issued.
-          </p>
-        </div>
-      );
-};
+// const About = () => {
+//       return (
+//         <div>
+//           <h3>1. Difference between javascript and nodejs</h3>
+//           <div className="d-flex mx-5">
+//             <div>
+//               {" "}
+//               <h1>JavaScript</h1>
+//               <p>
+//                 জাভাস্ক্রিপ্ট একটি প্রোগ্রামিং ভাষা যা ওয়েবসাইটে স্ক্রিপ্ট
+//                 লেখার জন্য ব্যবহৃত হয়।জাভাস্ক্রিপ্ট শুধুমাত্র ব্রাউজারে চালানো
+//                 যেতে পারে। এটি মূলত ক্লায়েন্ট-সাইডে ব্যবহৃত হয়। জাভাস্ক্রিপ্ট
+//                 এইচটিএমএল যোগ করতে যথেষ্ট সক্ষম।জাভাস্ক্রিপ্ট যেকোন ব্রাউজার
+//                 ইঞ্জিনে চলতে পারে যেমন সাফারিতে জেএস কোর এবং ফায়ারফক্সে
+//                 স্পাইডারমনকি।
+//               </p>
+//             </div>
+//             <div>
+//               <h1>Nodejs</h1>
+//               <p>
+//                 নোডজেএস একটি জাভাস্ক্রিপ্ট রানটাইম পরিবেশ| আমরা NodeJS এর
+//                 ​​সাহায্যে ব্রাউজারের বাইরে Javascript চালাতে পারি। এটি
+//                 বেশিরভাগই সার্ভার-সাইডে ব্যবহৃত হয়। এটি বেশিরভাগই সার্ভার-সাইডে
+//                 ব্যবহৃত হয়। Nodejs এর HTML ট্যাগ যোগ করার ক্ষমতা নেই। V8 হল
+//                 node.js এর ভিতরে জাভাস্ক্রিপ্ট ইঞ্জিন যা জাভাস্ক্রিপ্ট পার্স করে
+//                 এবং চালায়।
+//               </p>
+//             </div>
+//           </div>
+//           <h1>2.SQL vs NoSQL Database </h1>
+//           <p>
+//             এসকিউএল ডাটাবেসগুলি সম্পর্কযুক্ত, নোএসকিউএল ডেটাবেসগুলি সম্পর্কহীন।
+//             SQL ডাটাবেস স্ট্রাকচার্ড কোয়েরি ভাষা ব্যবহার করে এবং একটি
+//             পূর্বনির্ধারিত স্কিমা আছে। নোএসকিউএল ডাটাবেসে অসংগঠিত ডেটার জন্য
+//             গতিশীল স্কিমা রয়েছে। এসকিউএল ডাটাবেসগুলি উল্লম্বভাবে স্কেলযোগ্য,
+//             যখন নোএসকিউএল ডেটাবেসগুলি অনুভূমিকভাবে স্কেলযোগ্য। এসকিউএল
+//             ডাটাবেসগুলি টেবিল-ভিত্তিক, যখন নোএসকিউএল ডাটাবেসগুলি নথি, কী-মান,
+//             গ্রাফ, বা প্রশস্ত-কলাম স্টোর। এসকিউএল ডাটাবেসগুলি বহু-সারি লেনদেনের
+//             জন্য ভাল, যখন নথি বা JSON-এর মতো অসংগঠিত ডেটার জন্য NoSQL ভাল।
+//           </p>
+//           <h1>3.What is the purpose of jwt and how does it work ?</h1>
+//           <p>
+//             JWT, or JSON Web Token, is an open standard used to share security
+//             information between two parties — a client and a server. Each JWT
+//             contains encoded JSON objects, including a set of claims. JWTs are
+//             signed using a cryptographic algorithm to ensure that the claims
+//             cannot be altered after the token is issued.
+//           </p>
+//         </div>
+//       );
+// };
 
-export default About;
+// export default About;
