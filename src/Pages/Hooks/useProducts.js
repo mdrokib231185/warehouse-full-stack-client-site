e354e70b233dddb1c632191c1b2b2b26efa8047d
mdrokib231185 <mdrokhasan231185@gmail.com>
2022-05-09 12:52:02 +0600

    addd items add

--- src/Pages/Hooks/useProducts.js
+++ src/Pages/Hooks/useProducts.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-const useProducts = (productsId) => {
-  const [products, setProducts] = useState({});
-  useEffect(() => {
-    const url = `https://whispering-chamber-57446.herokuapp.com/products/${productsId}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, [productsId]);
-  return [products];
-};
+// const useProducts = (productsId) => {
+//   const [products, setProducts] = useState({});
+//   useEffect(() => {
+//     const url = `https://whispering-chamber-57446.herokuapp.com/products/${productsId}`;
+//     fetch(url)
+//       .then((res) => res.json())
+//       .then((data) => setProducts(data));
+//   }, [productsId]);
+//   return [products];
+// };
 
-export default useProducts;
+// export default useProducts;
