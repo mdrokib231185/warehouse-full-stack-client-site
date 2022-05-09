3c919eac8e84c223e3196e58c4c090f0dd1ae74e
mdrokib231185 <mdrokhasan231185@gmail.com>
2022-05-09 18:06:06 +0600

    all done

--- src/Pages/Login/SocialLogin/Social.js
+++ src/Pages/Login/SocialLogin/Social.js
@@ -1,36 +1,36 @@
-// import React from 'react';
-// import './Social.css'
-// import google from './../../../images/google.png'
-// import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-// import auth from '../../../Firebaseinit';
-// import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import './Social.css'
+import google from './../../../images/google.png'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../Firebaseinit';
+import { useNavigate } from 'react-router-dom';
 
-// const Social = () => {
-//   const navigate = useNavigate()
-//   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+const Social = () => {
+  const navigate = useNavigate()
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
-//   if (user) {
-//     navigate('/home')
-//   }
-//       return (
-//         <div>
-//           <div className="d-flex align-items-center">
-//             <div style={{ height: "1.5px" }} className="bg-dark w-50"></div>
-//             <p className="mt-2 px-2">or</p>
-//             <div style={{ height: "1.5px" }} className="bg-dark w-50"></div>
-//           </div>
+  if (user) {
+    navigate('/home')
+  }
+      return (
+        <div>
+          <div className="d-flex align-items-center">
+            <div style={{ height: "1.5px" }} className="bg-dark w-50"></div>
+            <p className="mt-2 px-2">or</p>
+            <div style={{ height: "1.5px" }} className="bg-dark w-50"></div>
+          </div>
           
 
-//           <button
-//             onClick={()=>signInWithGoogle ()}
+          <button
+            onClick={()=>signInWithGoogle ()}
             
-//             className="btn-bg w-50 mx-auto d-block text-center"
-//           >
-//             <img src={google} alt="" />
-//             <span className="px-2">Google Sign in</span>
-//           </button>
-//         </div>
-//       );
-// };
+            className="btn-bg w-50 mx-auto d-block text-center"
+          >
+            <img src={google} alt="" />
+            <span className="px-2">Google Sign in</span>
+          </button>
+        </div>
+      );
+};
 
-// export default Social;
+export default Social;
